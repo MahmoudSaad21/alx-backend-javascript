ee6c59f0424cf0cb5b12a956b8a22ad31c0354cb
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-07-22 21:56:39 +0000

    ..

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const var task = false;
-  const var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const var task = true;
-    const var task2 = false;
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
