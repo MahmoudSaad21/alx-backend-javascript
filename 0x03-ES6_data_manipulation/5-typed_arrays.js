004c307c718b8c3992457f7441b8fe7ccb54a575
MahmoudSaad21 <mahm0udsa3d404@gmail.com>
2024-07-30 14:37:29 +0000

    ..

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length || position < 0) {
-      throw Error('Position outside range');
+      throw new Error('Position outside range');
   }
-  const newBuffer = new ArrayBuffer(length);
-  const newView = new DataView(newBuffer, 0, length);
-  const newArray = new Int8Array(newBuffer);
-  newArray[position] = value;
-  return newView;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
